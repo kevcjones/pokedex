05e464a02e6d875d1b496bda1a942fd1d2a8669a
Kevin Jones <him@codebykev.dev>
2024-05-06 18:26:03 +0100

    Add search form to the search page

--- src/routes/search.tsx
+++ src/routes/search.tsx
@@ -12,7 +12,17 @@ function SearchComponent() {
         backgroundImage: "url(./pokehouse.jpg)",
       }}
     >
-      <div className="min-h-screen bg-zinc-900 bg-opacity-80"></div>
+      <div className="min-h-screen bg-zinc-900 bg-opacity-80 flex items-center justify-center flex-col">
+        <div className="w-4/5 md:w-2/3 lg:w-1/3 flex flex-col gap-2">
+          <input
+            type="text"
+            id="search"
+            placeholder="Search for a Pokemon..."
+            className="input w-full"
+          />
+          <button className="btn btn-primary btn-block">Let's go</button>
+        </div>
+      </div>
     </div>
   );
 }
