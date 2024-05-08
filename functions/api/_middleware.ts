c4ff71c39c19a08c56938a72701a89c00937cc8b
Kevin Jones <him@codebykev.dev>
2024-05-08 22:39:14 +0100

    Refactor middleware logic in _middleware.ts

--- functions/api/_middleware.ts
+++ functions/api/_middleware.ts
@@ -1,5 +1,5 @@
 export const onRequest: PagesFunction = async (context) => {
-  const req = context.request as Request;
+  // const req = context.request as Request;
 
   // if (req.method !== "GET" && req.url.indexOf("/api/asset?") !== -1)
   //   return context.next();
