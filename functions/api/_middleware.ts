f11296d6cf5dfbcf374d87aa2dd5d621f66510b4
Kevin Jones <him@codebykev.dev>
2024-05-09 22:58:06 +0100

    Fix middleware logic for asset API endpoint

--- functions/api/_middleware.ts
+++ functions/api/_middleware.ts
@@ -1,7 +1,7 @@
 export const onRequest: PagesFunction = async (context) => {
   const req = context.request as Request;
 
-  if (req.method !== "GET" && req.url.indexOf("/api/asset?") !== -1)
+  if (req.method == "GET" && req.url.indexOf("/api/asset?") !== -1)
     return context.next();
 
   const auth = req.headers.get("Authorization");
