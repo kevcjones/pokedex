841dc3b936282ebcbbf318b39883ac6a69e1e29b
Kevin Jones <him@codebykev.dev>
2024-05-09 22:55:12 +0100

    Refactor middleware logic in _middleware.ts

--- functions/api/_middleware.ts
+++ functions/api/_middleware.ts
@@ -1,16 +1,16 @@
 export const onRequest: PagesFunction = async (context) => {
-  // const req = context.request as Request;
+  const req = context.request as Request;
 
-  // if (req.method !== "GET" && req.url.indexOf("/api/asset?") !== -1)
-  //   return context.next();
+  if (req.method !== "GET" && req.url.indexOf("/api/asset?") !== -1)
+    return context.next();
 
-  // const auth = req.headers.get("Authorization");
+  const auth = req.headers.get("Authorization");
 
-  // if (auth !== "Example auth header") {
-  //   return new Response("Missing or invalid auth header", {
-  //     status: 401,
-  //   });
-  // }
+  if (auth !== "Example auth header") {
+    return new Response("Missing or invalid auth header", {
+      status: 401,
+    });
+  }
 
   return context.next();
 };
